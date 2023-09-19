9af4cfae51fde14ded7141e94361dccc18c76abf
sai charan kutikuppala <kutikuppalasaicharan97@gmail.com>
2023-09-19 21:03:39 +0530

    added auth variables in env.prod

--- src/env/env.prod.ts
+++ src/env/env.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
     production: true,
-    apiBaseUrl: "https://recipe-book-backend-n9lg.onrender.com"
+    apiBaseUrl: "https://recipe-book-backend-n9lg.onrender.com",
+    auth: {
+      redirect_uri: window.location.origin,
+      domain: 'dev-qlvesfqc.eu.auth0.com',
+      clientId: 'ZOMTwIeNvjF9qPb0B37JZhKfAlYFfy9H'
+    }
   };
